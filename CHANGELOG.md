a28648b3b822fd61551271daae49fdb05b3fb4b7
edlins <edlins@yahoo.com>
2018-05-27 19:40:28 +0000

    changelog, spectrum

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,14 +1,24 @@
 # Changelog
 
 
-## [0.7] - unreleased
+## [0.8] - unreleased
+### Added
+- **examples/spectrum**: audio example
+
+### Changed
+
+### Removed
+
+
+## [0.7] - 2018-05-27
 ### Added
 - **examples/olaclient/**: example application for driving a PCA9685 via DMX512 with
 test shell scripts
+- **netinst.sh**: build script suitable to be run as an EXTRA from `picontrol-netinst`
 
 ### Changed
-- **CMakeLists.txt**: build olaclient but excluded from all, must `make olaclient`
-- **.travis.yml**: install libola-dev on Trusty, add `make olaclient`
+- **.travis.yml**: install libola-dev on Trusty, add `make examples`
+- **[build]**: overhaul: exclude examples from default, every obj has own CMakeLists.txt
 
 ### Removed
 
---
+++ examples/spectrum/netinst.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+# executed by `post-install.txt` as `chroot /rootfs find /tmp/extras -type f -exec sh {} \;`
+# executed by user as `sudo ./netinst.sh`
+# plug in your usb sound card before running
+# it should appear as something like:
+#   $ aplay -L
+#   [...]
+#   plughw:CARD=Device,DEV=0
+#   C-Media USB Audio Device, USB Audio
+#   Hardware device with all software conversions
+
+
+echo ""
+echo "=== Adding spectrum ==="
+
+echo ""
+echo "= Turning audio on
+echo "dtparam=audio=on" >> /boot/config.txt
+
+echo ""
+echo "= Adding make"
+/usr/bin/apt-get -y --no-install-recommends install make
+
+# install cmake
+echo ""
+echo "= Adding cmake"
+/usr/bin/apt-get -y --no-install-recommends install cmake
+
+# install g++
+echo ""
+echo "= Adding g++"
+/usr/bin/apt-get -y --no-install-recommends install g++
+
+# install libasound2-dev
+echo ""
+echo "= Adding libasound2-dev"
+/usr/bin/apt-get -y --no-install-recommends install libasound2-dev
+
+# install alsa-utils
+echo ""
+echo "= Adding alsa-utils"
+/usr/bin/apt-get -y --no-install-recommends install alsa-utils
+
+# build and install libPCA9685
+cd /usr/local/src/libPCA9685/examples/spectrum
+echo ""
+echo "= Building spectrum
+mkdir build && cd build
+cmake ..
+make
+#echo ""
+#echo "= Installing spectrum"
+#make install
