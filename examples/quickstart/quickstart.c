c8f1ef52b9aaf7e402571d945240979cca6aa35a
Paul Vint <pjvint@gmail.com>
2019-02-06 13:16:24 -0500

    Allow initialization of multiple devices (#71)
    * leave /dev/i2c-1 as 0660
    
    * Changed "reset all" in initPWM() to just reset the selected device (also modified example to demonstrate second device usage)
    
    * Added comment info
    
    * Fixed info in comment
    
    * Updated info and fixed Travis CI test
    
    * capture log
    
    Travis CI build fails bc unit file not capturing stdout and stderr to /var/log/
    
    * revert, didn'
    
    * Update .travis.yml
    
    comment out olaclient log check.  olaclient seems to fail in Travis env..

--- examples/quickstart/quickstart.c
+++ examples/quickstart/quickstart.c
@@ -10,6 +10,9 @@
 
 int fd;
 int addr = 0x40;
+// Example for a second device. Set addr2 to device address (if set to 0x00 no second device will be used)
+int fd2;
+int addr2 = 0x00;
 
 
 void intHandler(int dummy) {
@@ -27,6 +30,7 @@ int initHardware(int adpt, int addr, int freq) {
 
 
 int main(void) {
+  //_PCA9685_DEBUG = 1;	// uncomment to show debug info
   fprintf(stdout, "quickstart %d.%d\n", libPCA9685_VERSION_MAJOR, libPCA9685_VERSION_MINOR);
   int adpt = 1;
   int freq = 200;
@@ -36,6 +40,15 @@ int main(void) {
     { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
   unsigned int setOffVals[_PCA9685_CHANS] =
     { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+  // DEVICE #2
+  if (addr2) {
+    fd2 = initHardware(adpt, addr2, freq);
+    unsigned int setOnVals2[_PCA9685_CHANS] =
+      { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    unsigned int setOffVals2[_PCA9685_CHANS] =
+      { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+  } // if addr2
 
   // blink endlessly 
   while (1) {
@@ -48,6 +61,18 @@ int main(void) {
     }
     // set the on and off vals on the PCA9685
     PCA9685_setPWMVals(fd, addr, setOnVals, setOffVals);
+
+    // DEVICE #2
+    if (addr2) {
+      for (i=0; i<_PCA9685_CHANS; i++) {
+        int random = rand();
+        int value = (float) random / (float) RAND_MAX * _PCA9685_MAXVAL;
+        setOffVals[i] = value;
+      }
+    
+      // set the on and off vals on the second PCA9685
+      PCA9685_setPWMVals(fd2, addr2, setOnVals, setOffVals);
+    } // if addr2
   }
   return 0;
 }
