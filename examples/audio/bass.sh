30f55cb39f3000a326546eede8c7cf387f6e76d5
edlins <edlins@yahoo.com>
2019-02-11 13:01:58 -0700

    script paths, cmdline for vupeak

--- examples/audio/bass.sh
+++ examples/audio/bass.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/usr/local/src/libPCA9685/build/examples/audio/vupeak -c 1 -B 2 -r 44100 -h -H 128 -p 128 -n 512
+/usr/local/bin/vupeak -c 1 -B 2 -r 44100 -h -H 128 -p 128 -n 512
--- examples/audio/fast.sh
+++ examples/audio/fast.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/usr/local/src/libPCA9685/build/examples/audio/vupeak -c 1 -B 2 -r 44100 -h -H 64 -p 64 -n 64
+/usr/local/bin/vupeak -c 1 -B 2 -r 44100 -h -H 64 -p 64 -n 64
